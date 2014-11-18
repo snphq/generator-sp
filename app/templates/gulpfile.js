240a93e7fa2676f96ba94dab7e109b0beb8ea2c1
Efremov Alexey <lexich121@gmail.com>
2014-11-18 11:38:43 +0300

    rename index.coffee to gulpfile.coffee

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,4 +1,4 @@
 // Note the new way of requesting CoffeeScript since 1.7.x
 require('coffee-script/register');
 // This bootstraps your Gulp's main file
-require('./gulp/index.coffee');
+require('./gulp/gulpfile.coffee');
