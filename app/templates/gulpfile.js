065371ff8e75faddf97a9c3952180ca944845f85
Maksim Koretskiy <maksim.koretskiy@saltpepper.ru>
2015-04-09 14:28:55 +0300

    Remove coffee from generated deps

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,5 +1,2 @@
-// Note the new way of requesting CoffeeScript since 1.7.x
-require('coffee-script/register');
-// This bootstraps your Gulp's main file
 var gulp = require("gulp");
 require('snp-gulp-tasks')(gulp);
